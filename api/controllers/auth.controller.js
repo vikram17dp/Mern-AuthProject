d64bd8278c153bbe7c866d0148f36219aa19c4e4
vikram17dp <vikramdp505@gmail.com>
2024-08-04 12:31:10 +0530

    create sigin API routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
+
 
 export const signup = async (req,res,next)=>{
     
@@ -12,6 +15,28 @@ export const signup = async (req,res,next)=>{
         
     } catch (error) {
         // next(errorHandler(500,"something went wrong"))
+        next(error)
+    }
+}
+export const signin = async (req,res,next)=>{
+
+    const {email,password} = req.body;
+    try {
+        const validUser = await  User.findOne({email});
+        if(!validUser) return next(errorHandler(404,"User not found"));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,"wrong credentials"));
+        if (!process.env.JWT_SECRET) {
+            return next(errorHandler(500, "JWT_SECRET is not defined"));
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:hashedpassword,...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}),
+        res.status(200),
+        res.json(rest);
+        
+    } catch (error) {
         next(error)
     }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
+import cookieParser from 'cookie-parser';
+import { configDotenv } from "dotenv";
 
 const router = express.Router();
 
+
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 export default router;
