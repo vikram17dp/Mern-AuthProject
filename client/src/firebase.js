a64029c1edf7743a33a610b5a5df8e1a97a0ac1b
vikram17dp <vikramdp505@gmail.com>
2024-08-31 00:37:27 +0530

    profile page and firebase setup

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Import Firebase Authentication if needed
+import { getFirestore } from "firebase/firestore"; // Import Firestore if needed
+import { getStorage } from "firebase/storage"; // Import Firebase Storage if needed
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); // Firebase Authentication
+export const db = getFirestore(app); // Firestore Database
+export const storage = getStorage(app); // Firebase Storage
+
+export default app;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,86 @@
-import React from 'react'
-import {useSelector} from'react-redux'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useRef } from "react";
+import { ref, getStorage, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import {app} from '../firebase'
 
 export default function Profile() {
-  const {currentUser} = useSelector(state => state.user)
- 
+  const fileref = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(formData);
+  
+  useEffect(()=>{
+    if(image){
+      handleFileUpload(image);
+    }
+  },[image])
+const handleFileUpload = async (image)=>{
+      const storage = getStorage(app);
+      const filename = new Date().getTime() + image.name;
+      const storageRef = ref(storage,filename);
+      const uploadTask = uploadBytesResumable(storageRef,image);
+      
+      uploadTask.on('state_changed',(snapshot)=>{
+        const progress = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+       setImagePercent(Math.round(progress));
+
+      },
+      (error)=>{
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    )
+}
   return (
-    <div className='p-3 max-w-lg mx-auto gap-4'>
-      <h1 className='text-3xl text-center my-7 font-semibold'>Profile</h1>
-      <form action="" className='flex flex-col'>
-      <img src={currentUser.profileImage} className='h-24 w-24 self-center ' alt="" />
-      <input defaultValue={currentUser.username} type="text" placeholder='Username' id='username' className='p-3 bg-slate-300  mt-3 rounded-lg cursor-pointer' />
-      <input defaultValue={currentUser.email} type="email" placeholder='email'id='email' className='bg-slate-300 p-3 mt-3 rounded-lg' />
-      <input type="password" placeholder='password' id='password' className='bg-slate-300 p-3 rounded-lg mt-3' />
-      <button className='bg-slate-700 text-white uppercase mt-3 p-3 rounded-lg opacity-95 hover:opacity-80'>Update</button>
-      <div className='justify-between flex mt-3'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
-      </div>
+    <div className="p-3 max-w-lg mx-auto gap-4">
+      <h1 className="text-3xl text-center my-7 font-semibold">Profile</h1>
+      <form action="" className="flex flex-col">
+        <input type="file" ref={fileref} hidden accept="image/*"
+          onChange={(e)=>setImage(e.target.files[0])}
+        />
+        <img
+          src={currentUser.profileImage}
+          className="h-24 w-24 self-center cursor-pointer "
+          alt=""
+          onClick={() => fileref.current.click()}
+        />
+        
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="p-3 bg-slate-300  mt-3 rounded-lg cursor-pointer"
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-300 p-3 mt-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-300 p-3 rounded-lg mt-3"
+        />
+        <button className="bg-slate-700 text-white uppercase mt-3 p-3 rounded-lg opacity-95 hover:opacity-80">
+          Update
+        </button>
+        <div className="justify-between flex mt-3">
+          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <span className="text-red-700 cursor-pointer">Sign Out</span>
+        </div>
       </form>
     </div>
-  )
+  );
 }
